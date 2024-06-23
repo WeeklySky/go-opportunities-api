96b1c46d2afdfd6217440d8c2b332dc4cd83b085
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2024-06-23 17:55:21 -0300

    docs: added documentation for delete opening

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -7,6 +7,18 @@ import (
 	"github.com/guilhermemcardoso/go-opportunities-api/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Delete opening
+// @Description Delete an existing job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening ID"
+// @Success 200 {object} DeleteOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [delete]
 func DeleteOpeningHandler(ctx *gin.Context) {
 
 	id := ctx.Query("id")
