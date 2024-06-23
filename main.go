3a06e99298295590cf261ad2d81c18e8f7bc5353
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2024-06-23 12:49:00 -0300

    feat: added custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/guilhermemcardoso/go-opportunities-api/router"
+import (
+	"github.com/guilhermemcardoso/go-opportunities-api/config"
+	"github.com/guilhermemcardoso/go-opportunities-api/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	// initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config init error: %v", err)
+		return
+	}
 
 	// initialize router
 	router.Initialize()
